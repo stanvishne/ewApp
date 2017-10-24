c5b058f0aebae189f97f32a13e74f83cffc09629
Stanislav Vishnevetskiy <svishnev@svishnev01-mac.local>
2017-10-24 15:51:44 +0300

    structure changes

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express   = require('express');
 const app       = express();
 const port      = 3000;
 
-var cors = require('cors')
-app.use(cors());
-app.use(express.static(`${__dirname}./../client/public/`)); 		// statics
-require(`./src/routes/routes.js`)(app);						// routes
+//var cors = require('cors')
+//app.use(cors());
+app.use(express.static(`${__dirname}/client/public/`)); 		// statics
+require(`./server/routes/routes.js`)(app);						// routes
 
 app.listen(port);										// let the games begin!
 console.log(`Web server listening on port ${port}`);
