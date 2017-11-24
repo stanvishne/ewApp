a57256a3e1872d6f6f75c91032a1c84530b882c6
stanvishne <shlomo-stanisla.vishnevetskiy@amdocs.com>
2017-11-24 19:20:45 +0200

    pipe

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,5 +1,7 @@
 import React from "react";
 //import UserList from "./common/UserList";
+import Pipe from './Pipe/Pipe';
+
 
 // Home page component
 export default class Home extends React.Component {
@@ -8,6 +10,7 @@ export default class Home extends React.Component {
     return (
       <div className="page-home">
         Home
+        <Pipe/>
       </div>
     );
   }
